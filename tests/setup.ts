76beaedd6793988b179e55161f6e0c41d81bad37
Arni <arnifa@gmail.com>
2025-02-23 09:07:08 +0000

    Adding test framework

---
+++ tests/setup.ts
@@ -0,0 +1,9 @@
+import { beforeAll, afterAll, afterEach } from 'vitest'
+import { setupServer } from 'msw/node'
+import { handlers } from './mocks/handlers'
+
+export const server = setupServer(...handlers)
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
